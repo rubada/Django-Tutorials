29960d351f72f7658a4c85d559c6ac9f60ee2b9e
Ruba Dabbas <ruba@al-dabbas.com>
2026-02-02 01:48:46 +0300

    add pro03

--- starting_project.py
+++ starting_project.py
@@ -20,3 +20,13 @@
 
 # 6. Make a new app.
 # python manage.py startapp app-name
+
+# Creating the app:
+# 1. Add the app URL in the main "urls.py" in the project folder.
+# 2. Add the app name to the "INSTALLED_APPS" in the settings.py file.
+# 3. Create a "urls.py" file in your app
+# 4. After creating the view, add its URL to the app "urls.py" file.
+
+# Create templates:
+# 1. Create the "templates" folder.
+# 2. Add the "templates" folder path in the templates list in settings.py.
